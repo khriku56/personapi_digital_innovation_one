797e11085fd41418086330c647efa8169559be92
khriku56 <43750128+khriku56@users.noreply.github.com>
2022-01-22 15:48:50 -0300

    adding new method rg

--- src/test/java/one/digitalinnovation/personapi/utils/PersonUtils.java
+++ src/test/java/one/digitalinnovation/personapi/utils/PersonUtils.java
@@ -11,6 +11,7 @@ public class PersonUtils {
     private static final String FIRST_NAME = "Rodrigo";
     private static final String LAST_NAME = "Peleias";
     private static final String CPF_NUMBER = "369.333.878-79";
+    private static final String RG = "71.034.360-0";
     private static final long PERSON_ID = 1L;
     public static final LocalDate BIRTH_DATE = LocalDate.of(2010, 10, 1);
 
@@ -19,6 +20,7 @@ public class PersonUtils {
                 .firstName(FIRST_NAME)
                 .lastName(LAST_NAME)
                 .cpf(CPF_NUMBER)
+                .rg(RG)
                 .birthDate("04-04-2010")
                 .phones(Collections.singletonList(PhoneUtils.createFakeDTO()))
                 .build();
@@ -30,6 +32,7 @@ public class PersonUtils {
                 .firstName(FIRST_NAME)
                 .lastName(LAST_NAME)
                 .cpf(CPF_NUMBER)
+                .rg(RG)
                 .birthDate(BIRTH_DATE)
                 .phones(Collections.singletonList(PhoneUtils.createFakeEntity()))
                 .build();
